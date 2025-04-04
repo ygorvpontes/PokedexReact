a8d1188e291bbc145891465bf14094d0d437884f
ygorvpontes <161664460+ygorvpontes@users.noreply.github.com>
2025-04-04 07:32:51 -0300

    fist

---
+++ PokedexReact/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+
+
+function App() {
+  const [allpokemon, setAllpokemon] = useState([])
+  const [searchTerms, setSearchTerm] = useState('')
+  
+  useEffect(()=>{
+    const getpokemon = async ()=>{
+      
+    }
+  }
+    
+  )
+
+
+
+
+  return (
+  
+      <>
+      </>
+    
+  )
+}
+
+export default App
